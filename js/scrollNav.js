8a9e17c483e6621be0005c685bf880a8a4262e42
cybershubham <shubhamjain1602+github@gmail.com>
2015-04-09 21:50:57 +0530

    added event.html \n made contact.html; fixed navbar and navbar-brand; matched background

--- js/scrollNav.js
+++ js/scrollNav.js
@@ -5,7 +5,7 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top-123
+          scrollTop: target.offset().top-148
         }, 1000);
         return false;
       }
